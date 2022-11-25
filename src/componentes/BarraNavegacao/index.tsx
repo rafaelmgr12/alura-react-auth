3db0fcfac5cbd37d1fec2282414eddca5e5cb692
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2022-11-25 12:59:16 -0300

    feat: login modal

--- src/componentes/BarraNavegacao/index.tsx
+++ src/componentes/BarraNavegacao/index.tsx
@@ -1,71 +1,73 @@
-import { useState } from "react"
-import { Link } from "react-router-dom"
-import BotaoNavegacao from "../BotaoNavegacao"
-import ModalCadastroUsuario from "../ModalCadastroUsuario"
-import logo from './assets/logo.png'
-import usuario from './assets/usuario.svg'
-import './BarraNavegacao.css'
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import BotaoNavegacao from "../BotaoNavegacao";
+import ModalCadastroUsuario from "../ModalCadastroUsuario";
+import ModalLoginUsuario from "../ModalLoginUsuario";
+import logo from "./assets/logo.png";
+import usuario from "./assets/usuario.svg";
+import "./BarraNavegacao.css";
 
 const BarraNavegacao = () => {
-    const [modalCadastroAberta, setModalCadastroAberta] = useState(false)
+  const [modalCadastroAberta, setModalCadastroAberta] = useState(false);
+  const [modalLoginAberta, setModalLoginAberta] = useState(false);
 
-    return (<nav className="ab-navbar">
-        <h1 className="logo">
-            <Link to="/">
-                <img className="logo" src={logo} alt="Logo da AluraBooks" />
-            </Link>
-        </h1>
-        <ul className="navegacao">
+  return (
+    <nav className="ab-navbar">
+      <h1 className="logo">
+        <Link to="/">
+          <img className="logo" src={logo} alt="Logo da AluraBooks" />
+        </Link>
+      </h1>
+      <ul className="navegacao">
+        <li>
+          <a href="#!">Categorias</a>
+          <ul className="submenu">
             <li>
-                <a href="#!">Categorias</a>
-                <ul className="submenu">
-                    <li>
-                        <Link to="/">
-                            Frontend
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/">
-                            Programação
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/">
-                            Infraestrutura
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/">
-                            Business
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/">
-                            Design e UX
-                        </Link>
-                    </li>
-                </ul>
-            </li>
-        </ul>
-        <ul className="acoes">
-            <li>
-                <BotaoNavegacao texto="Login" textoAltSrc="Icone representando um usuário" imagemSrc={usuario}/>
+              <Link to="/">Frontend</Link>
             </li>
             <li>
-            <BotaoNavegacao
-                texto="Cadastrar-se"
-                textoAltSrc="Icone representando um usuário"
-                imagemSrc={usuario}
-                onClick={() => setModalCadastroAberta(true)}
-            />
-            <ModalCadastroUsuario
-                aberta={modalCadastroAberta}
-                aoFechar={() => setModalCadastroAberta(false)}
-            />
+              <Link to="/">Programação</Link>
             </li>
-            
-        </ul>
-    </nav>)
-}
+            <li>
+              <Link to="/">Infraestrutura</Link>
+            </li>
+            <li>
+              <Link to="/">Business</Link>
+            </li>
+            <li>
+              <Link to="/">Design e UX</Link>
+            </li>
+          </ul>
+        </li>
+      </ul>
+      <ul className="acoes">
+        <li>
+          <BotaoNavegacao
+            texto="Login"
+            textoAltSrc="Icone representando um usuário"
+            imagemSrc={usuario}
+            onClick={() => setModalLoginAberta(true)}
+          />
+          <ModalLoginUsuario
+            aberta={modalLoginAberta}
+            aoFechar={() => setModalLoginAberta(false)}
+          />
+        </li>
+        <li>
+          <BotaoNavegacao
+            texto="Cadastrar-se"
+            textoAltSrc="Icone representando um usuário"
+            imagemSrc={usuario}
+            onClick={() => setModalCadastroAberta(true)}
+          />
+          <ModalCadastroUsuario
+            aberta={modalCadastroAberta}
+            aoFechar={() => setModalCadastroAberta(false)}
+          />
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
-export default BarraNavegacao
+export default BarraNavegacao;
